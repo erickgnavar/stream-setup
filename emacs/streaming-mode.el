b9f4e457057a1be5621cbea828957b9181be97d1
Erick Navarro <erick@navarro.io>
2022-09-03 20:37:11 -0500

    [emacs] Add streaming-mode.el
    Configure emacs for better default for streaming and to interact with
    overlay application

---
+++ emacs/streaming-mode.el
@@ -0,0 +1,71 @@
+;;; streaming-mode.el --- Tools for streaming
+
+;;; Commentary:
+;; Collection of tools to integrate Emacs with elixir overlay application
+
+;;; Code:
+
+(require 'erc)
+
+(defcustom streaming-mode--font-family "Iosevka"
+  "Font used for streaming."
+  :type 'string
+  :group 'streaming-mode)
+
+(defcustom streaming-mode--overlay-url "http://localhost:4000/api/overlay"
+  "Overlay endpoint to send notifications."
+  :type 'string
+  :group 'streaming-mode)
+
+(defvar streaming-mode--big-font-mode-tmp)
+
+(define-minor-mode streaming-mode--big-font-mode
+  "Switch between a regular font size and a presentation font size."
+  :init-value nil
+  :global t
+  :group 'streaming-mode
+  (if streaming-mode--big-font-mode
+      (progn
+        ;; save current font size in a temp variable to be able to restore it
+        ;; after this minor mode is disabled
+        (setq streaming-mode--big-font-mode-tmp (/ (face-attribute 'default :height) 10))
+        (set-frame-font (format "%s %d" streaming-mode--font-family 26)))
+    (set-frame-font (format "%s %d" streaming-mode--font-family streaming-mode--big-font-mode-tmp))))
+
+(defun streaming-mode--event-post (event-name)
+  "Make POST request using 'EVENT-NAME' to notify overlay application."
+  (let ((url-request-method "POST")
+        (url-request-extra-headers `(("Content-Type" . "application/json")))
+        (url-request-data (format "{\"event\": \"%s\"}" event-name)))
+    (url-retrieve streaming-mode--overlay-url (lambda (status) nil))))
+
+(defun streaming-mode--notify-overlay (_buffer result)
+  "Check compilation RESULT and trigger a event for each case."
+  (if (string-match "^finished" result)
+      (streaming-mode--event-post "exit_zero")
+    (streaming-mode--event-post "exit_non_zero")))
+
+(define-minor-mode streaming-mode
+  "Configuration for streaming."
+  :init-value nil
+  :lighter "Streaming mode"
+  :global t
+  :group 'streaming-mode
+  (if streaming-mode
+      (progn
+        (add-to-list 'compilation-finish-functions 'streaming-mode--notify-overlay)
+        (streaming-mode--big-font-mode))
+    (progn
+      (setq compilation-finish-functions (delq 'streaming-mode--notify-overlay compilation-finish-functions))
+      (streaming-mode--big-font-mode -1))))
+
+(defun streaming-mode-setup-twitch-irc ()
+  "Setup ERC variables to connect to twitch IRC server."
+  (interactive)
+  (setq erc-server "irc.chat.twitch.tv"
+        erc-nick "erickgnavar"
+        erc-password (read-passwd "Enter Twitch IRC token: ")))
+
+(provide 'streaming-mode)
+
+;;; streaming-mode.el ends here
