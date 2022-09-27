dcac3dd1c0456f4eb2ff657a11269af923b769fd
Erick Navarro <erick@navarro.io>
2022-09-27 17:30:05 -0500

    [alfred] Add overlay notification for light theme reward
    Also reduce notification title font size

--- alfred/lib/alfred/twitch/rewards_handler.ex
+++ alfred/lib/alfred/twitch/rewards_handler.ex
@@ -17,6 +17,18 @@ defmodule Alfred.Twitch.RewardsHandler do
       AlfredWeb.EmacsChannel.pubsub_topic(),
       {:send_event, "light-theme", %{user: username}}
     )
+
+    case Alfred.Core.get_config_param("rewards.light_theme") do
+      nil ->
+        nil
+
+      %{value: url} ->
+        Phoenix.PubSub.broadcast(
+          Alfred.PubSub,
+          AlfredWeb.OverlayLive.topic_name(),
+          {:new_notification, %{title: "#{username} canjeó light theme", image_url: url}}
+        )
+    end
   end
 
   def handle("burn code", _username, _text) do
