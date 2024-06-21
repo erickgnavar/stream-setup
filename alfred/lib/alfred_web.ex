8c6907a413394ac40fd871233c71c36acea1484d
Erick Navarro <erick@navarro.io>
2024-06-21 11:19:17 -0600

    [alfred] Upgrade to phoenix 1.7
    Make changes to fix new version conventions and format all the HTML
    code using new plugin Phoenix.LiveView.HTMLFormatter

--- alfred/lib/alfred_web.ex
+++ alfred/lib/alfred_web.ex
@@ -45,7 +45,7 @@ defmodule AlfredWeb do
   def live_view do
     quote do
       use Phoenix.LiveView,
-        layout: {AlfredWeb.LayoutView, "live.html"}
+        layout: {AlfredWeb.LayoutView, :live}
 
       unquote(view_helpers())
     end
@@ -88,6 +88,7 @@ defmodule AlfredWeb do
     quote do
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
+      use Phoenix.Component
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
--- alfred/mix.exs
+++ alfred/mix.exs
@@ -33,15 +33,15 @@ defmodule Alfred.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.7"},
+      {:phoenix, "~> 1.7"},
+      {:phoenix_view, "~> 2.0"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:ecto_sqlite3, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17.5"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.6"},
+      {:phoenix_live_view, "~> 0.20.17"},
+      {:phoenix_live_dashboard, "~> 0.8.4"},
       {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
