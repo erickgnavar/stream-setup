695b54f55ccd38b3bf8cfbae2f40505671921187
Erick Navarro <erick@navarro.io>
2025-07-22 12:34:11 -0600

    [alfred] Update to elixir 1.18

--- alfred/mix.exs
+++ alfred/mix.exs
@@ -5,7 +5,7 @@ defmodule Alfred.MixProject do
     [
       app: :alfred,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
